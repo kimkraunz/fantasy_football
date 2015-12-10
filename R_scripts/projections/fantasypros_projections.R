92aa52918d56c6ce7c3ba7333de14dea5e394484
kimkraunz <kim.kraunz@gmail.com>
2015-12-10 13:47:33 -0800

    updated- fully running

--- R_scripts/projections/fantasypros_projections.R
+++ R_scripts/projections/fantasypros_projections.R
@@ -1,5 +1,4 @@
-# WORKS BUT NEED TO FIX NAMES IF GOING TO USE IN BAYESIAN ANALYSIS
-# Also need to remove optimizer at end
+
 
 library("XML")
 library("stringr")
@@ -28,90 +27,39 @@ wr_fp <- mutate(wr_fp, pos = "WR")
 rb_fp <- mutate(rb_fp, pos = "RB")
 te_fp <- mutate(te_fp, pos = "TE")
 k_fp <- mutate(k_fp, pos = "K")
-dst_fp <- mutate(dst_fp, pos = "D")
+dst_fp <- mutate(dst_fp, pos = "DST")
 
 # Merge position tables into one data frame
-df_fp <- rbind_list(qb_fp, wr_fp, rb_fp, te_fp, k_fp, dst_fp)
+projections_fp <- rbind_list(qb_fp, wr_fp, rb_fp, te_fp, k_fp, dst_fp)
 
 # Keep only Player, Proj.Points, Salary, and pos columns
-df_fp <- select(df_fp, Player, Proj.Points, Salary, pos)
+projections_fp <- select(projections_fp, Player, Proj.Points, Salary, pos)
 
-# Clean up names
-df_fp$Player <- str_replace(df_fp$Player, " \\(.*", "")
-df_fp$first_name <- lapply(strsplit(as.character(df_fp$Player), " "), "[", 1)
-df_fp$last_name <- lapply(strsplit(as.character(df_fp$Player), " "), "[", 2)
+# Extract team from Player
+projections_fp$Team<- lapply(strsplit(as.character(projections_fp$Player), "\\(+"), "[", 2)
+projections_fp$Team<- lapply(strsplit(as.character(projections_fp$Team), "\\s+"), "[", 1)
+projections_fp$Team <- as.factor(unlist(projections_fp$Team))
 
-df_fp$first_name <- toupper(df_fp$first_name)
-df_fp$last_name <- toupper(df_fp$last_name)
+# Clean up Player
+projections_fp$first_name <- lapply(strsplit(as.character(projections_fp$Player), " "), "[", 1)
+projections_fp$last_name <- lapply(strsplit(as.character(projections_fp$Player), " "), "[", 2)
+projections_fp$first_name <- toupper(projections_fp$first_name)
+projections_fp$last_name <- toupper(projections_fp$last_name)
 
-df_fp$Player <- paste0(df_fp$last_name, df_fp$first_name, sep = "")
-
+projections_fp$Player <- paste0(projections_fp$last_name, projections_fp$first_name, sep = "")
 
 #Remove special characters
-df_fp$Salary <- gsub("[[:punct:]]", "", df_fp$Salary)
-df_fp$Proj.Points <- gsub(" pts", "", df_fp$Proj.Points)
-df_fp$Player <- gsub("[[:punct:]]", "", df_fp$Player)
+projections_fp$Proj.Points <- gsub(" pts", "", projections_fp$Proj.Points)
+projections_fp$Player <- gsub("[[:punct:]]", "", projections_fp$Player)
 
-df_fp$pos <- as.factor(df_fp$pos)
-df_fp$Proj.Points <- as.numeric(df_fp$Proj.Points)
-df_fp$Salary <- as.numeric(df_fp$Salary)
+projections_fp$pos <- as.factor(projections_fp$pos)
+projections_fp$Proj.Points <- as.numeric(projections_fp$Proj.Points)
 
-df_fp <- select(df_fp, Player, Proj.Points, Salary, pos)
-colnames(df_fp) <- c("Player", "Points", "Cost", "Position")
+# add source column
+projections_fp$proj.source <- "fantasypros"
+
+projections_fp <- select(projections_fp, Player, Proj.Points, Team, pos)
+colnames(projections_fp) <- c("Player", "Points", "Team", "Position")
 
 # write projected points to csv
-write.csv(df_fp, file = paste0(getwd(), "/fantasy_football/projections_data/fp_projections_", Sys.Date(), ".csv", sep = ""))
-
-df_fp <- df_fp[df_fp$Player != "New York Jets (NYJ - DST)", ]
-df_fp <- df_fp[1:317, ]
-
-df_fp <- df_fp[df_fp$Player != "Allen Robinson (JAC - WR)", ]
-
-Opt.squad<-function(dat,cost,n.d=1,n.k=1,n.qb=1,n.rb=2, n.wr=3, n.te=1) {
-    
-    ##Create dummy variables
-    fact.only<-df_fp[, which(sapply(df_fp, class)=="factor")]
-    non.fact<-df_fp[, which(sapply(df_fp, class)!="factor")]
-    dat.dummy<-model.matrix(~ Position, data=fact.only, contrasts.arg=list(Position=contrasts(fact.only$Position, contrasts=F)))
-    
-    
-    dat2<-dat.dummy[,-1]
-    colnames(dat2)<-gsub("[^#A-Za-z0-9_]","",colnames(dat2))
-    dat<-cbind(non.fact,dat2)
-    ##Pick optimum team
-    f.obj <- dat$Points
-    nn<-length(f.obj)
-    num_play  <- rep(1,nn)
-    n.choose<-n.d+n.k+n.qb+n.rb+n.wr+n.te
-    f.con <- cbind(Cost=dat$Cost,dat[,colnames(dat) %in% colnames(dat2)],num_play)
-    f.con<-t(f.con)
-    f.dir <- c("<=","=","=","=","=","=", "=", "=")
-    f.rhs <- c(cost,n.d,n.k,n.qb,n.rb,n.te,n.wr,n.choose)
-    
-    out1  <- lp("max", f.obj, f.con, f.dir, f.rhs,all.bin=TRUE)
-    
-    if (out1$status == 2) {
-        print ("There was no optimal solution. Try a different approach or increase/decrease your total cost")
-    } else {
-        sol1  <- out1$solution
-        total1 <- out1$objval 
-        ###Take numbers and take out 
-        pick1a <- rep(0,15)
-        k     <- 0
-        f.obj2<-f.obj
-        for (j in 1:nn) {
-            if (sol1[j]==1) {
-                k        <- k+1
-                pick1a[k] <- j
-            }
-        }
-        final.pick<-dat[pick1a, 1:3]
-        print(total1)
-        final.pick
-    }
-}
-
-Opt.squad(df_fp, 60000)
-
-df <- read.csv("/Users/kimkraunz/Documents/fantasy_football/fp_projections_09242015.csv", header = TRUE)
-Opt.squad(df, 60000)
+write.csv(projections_fp, file = paste0(getwd(), "/fantasy_football/projections_data/fp_projections_", Sys.Date(), ".csv", sep = ""))
